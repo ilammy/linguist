497d4d3ad4f0fdae1faa7535dc98625282e8c907
ilammy <me@ilammy.net>
2021-06-09 19:29:56 +0900

    verbatim https://github.com/J3T4R0/querkles/blob/6af974e79ea175f61b8628c853b4608878834589/uptown.html.pm

---
+++ samples/Pollen/uptown.html.pm
@@ -0,0 +1,21 @@
+#lang pollen
+◊(define my-inset "30%")
+<body style="margin: ◊my-inset; border:1px solid black">
+◊my-inset is the inset.
+</body>
+
+
+◊(define metal "Plutonium")
+
+Pollen + ◊metal
+---------------
+
++ You **wanted** ◊metal — you _got_ it.
+
++ [search for ◊metal](https://google.com/search?q=◊metal)
+◊headline{Pollen markup}
+
+◊items{
+  ◊item{You ◊strong{wanted} it — you ◊em{got} it.}
+  ◊item{◊link["https://google.com/search?q=racket"]{search for Racket}}
+}
