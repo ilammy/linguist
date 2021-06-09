00fbcea263f161a46c897b754ee631057eeaa164
ilammy <me@ilammy.net>
2021-06-09 19:25:44 +0900

    verbatim https://github.com/sanchom/sanchom.github.io/blob/6985ec9f9072ae5b496e53484a4917a313d7611c/index.html.pm

---
+++ samples/Pollen/index.html.pm
@@ -0,0 +1,76 @@
+#lang pollen
+
+◊define-meta[page-title]{Home}
+
+I just completed 3L at UBC's Allard School of Law. I spent my final
+term at the Indigenous Community Legal Clinic. In summer 2021, I am
+working on some personal research and writing projects and doing some
+volunteer work. Long term, I hope to do work that gives more of us
+more confidence in the outcomes of the legal system and government
+decision-making in general. I am very interested in the many overlaps
+of law and technology.
+
+I was on the [Gale Moot](https://www.galecupmoot.com/) team, was the
+Senior Editor at the [Canadian Journal of Family
+Law](http://faculty.allard.ubc.ca/cdnjfl/), and organized a reading
+group on [Algorithmic Decision-Making and the Rule of
+Law](https://github.com/sanchom/algorithmic-decision-making-and-rule-of-law).
+
+I'm trained as a computer scientist. In that field, I've focused on
+visual object recognition, machine learning, and applications of deep
+neural networks.
+
+If you'd like to get updates from me whenever I write something,
+◊outline{[sign up for my tinyletter
+here](https://tinyletter.com/sanchom)}.
+
+Here's where you can find me elsewhere online: ◊div[#:class
+"external-link-logos"]{ ◊little-logo[#:href
+"https://sanchom.wordpress.com" #:img
+"assets/wordpress-logo-notext-rgb.png"] ◊little-logo[#:href
+"https://twitter.com/sanchom" #:img "assets/tw_ico.png"]
+◊little-logo[#:href "mailto:sanchom@gmail.com" #:img
+"assets/email.png"] }
+
+◊heading{Selected publications}
+
+◊format-work[#:type "article" #:author-given "Sancho" #:author-family
+"McCann" #:title "Copyright Throughout a Creative AI Pipeline"
+#:forthcoming "2021" #:journal "Can J Law & Tech" #:volume "19"]
+
+◊format-work[#:type "article" #:author-given "Sancho" #:author-family
+"McCann" #:title "Finding Harmony: *Law Society of British Columbia v
+Trinity Western University*" #:comment-info "Case Comment" #:journal
+"Dalhousie Journal of Legal Studies" #:volume "28" #:year "2019"
+#:first-page "95" #:url
+"https://commentary.canlii.org/w/canlii/2019CanLIIDocs2940"]
+
+◊format-work[#:type "thesis" #:title "Object classification and
+localization using spatially-localized features" #:author-given
+"Sancho" #:author-family "McCann" #:thesis-description "PhD
+Dissertation" #:institution "University of British Columbia" #:year
+"2014" #:url
+"https://open.library.ubc.ca/media/download/pdf/24/1.0167312/1"]
+
+◊format-work[#:type "proceedings" #:author-given "Sancho"
+#:author-family "McCann" #:author2-given "David G" #:author2-family
+"Lowe" #:title "Spatially-local coding for object recognition"
+#:proceedings "11th Asian Conference on Computer Vision" #:year "2012"
+#:first-page "204" #:publisher "Springer" #:url
+"assets/McCannLowe_ACCV2012_0851.pdf"]
+
+◊format-work[#:type "proceedings" #:author-given "Sancho"
+#:author-family "McCann" #:author2-given "David G" #:author2-family
+"Lowe" #:title "Local naive Bayes nearest neighbor for image
+classification" #:proceedings "Computer Vision and Pattern
+Recognition" #:publisher "IEEE" #:year "2012" #:url
+"assets/McCannLowe_CVPR2012_1925.pdf" #:first-page "3650"]
+
+◊format-work[#:type "unpublished" #:author-given "Sancho"
+#:author-family "McCann" #:title "Atmospheric Sounding Visualization"
+#:url "assets/McCann-AtmosView.pdf" #:description "course report,
+Information Visualization, Department of Computer Science, University
+of British Columbia" #:year "2006"]
+
+A more complete list is at my [Google Scholar
+profile](https://scholar.google.com/citations?user=6C-udIUAAAAJ).
